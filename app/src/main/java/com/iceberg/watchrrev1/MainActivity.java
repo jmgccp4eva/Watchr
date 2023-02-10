76fa1a01920c2976309af6237b0a911606ca1a23
jmgccp4eva <jasonmgrebe@gmail.com>
2023-02-10 15:06:19 -0600

    Initial commit

---
+++ app/src/main/java/com/iceberg/watchrrev1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.iceberg.watchrrev1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this,SignUp.class);
+        startActivity(intent);
+        finish();
+    }
+}
